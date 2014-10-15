e688250d9829b49e5c41dfe04071d0883ce4373c
apriliakartika <aprilia.kartika.s@mail.ugm.ac.id>
2014-10-15 08:53:47 +0700

    Add login

---
+++ login/client.php
@@ -0,0 +1,19 @@
+<?php
+//memulai session 
+session_start();
+//panggil library 
+require_once('nusoap/lib/nusoap.php');
+//mendefinisikan alamat url service yang disediakan oleh client
+$client = new nusoap_client('http://localhost/ws/login/server.php?wsdl',true);
+$username = $_POST["username"];
+$password = $_POST["password"];
+$result  =  $client->call('login_ws',
+array('username'=>$username,
+'password'=>$password));
+if($result == "Login Berhasil"){
+$_SESSION['username'] = $username;
+header ("location:index.php");
+} else{
+header ("location:login.php");
+}
+?>
